41fdf76aeff742764623e6617e32470dcd581b42
Eganin <egorzaharin12@gmail.com>
2022-11-04 16:14:28 +0300

    init project

---
+++ app/src/main/java/com/best/shopmobileapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.best.shopmobileapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.best.shopmobileapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
