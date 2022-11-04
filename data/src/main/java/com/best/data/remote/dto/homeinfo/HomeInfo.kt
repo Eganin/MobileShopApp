b61135dfa5f057bca33ec945ab41e1c0e5ac2ca4
Eganin <egorzaharin12@gmail.com>
2022-11-04 16:25:18 +0300

    add dto objects

---
+++ data/src/main/java/com/best/data/remote/dto/homeinfo/HomeInfo.kt
@@ -0,0 +1,10 @@
+package com.best.data.remote.dto.homeinfo
+
+import com.google.gson.annotations.SerializedName
+
+data class HomeInfo(
+
+    @SerializedName("home_store") var homeStore: List<HomeStore> = emptyList(),
+    @SerializedName("best_seller") var bestSeller: List<BestSeller> = emptyList()
+
+)
---
+++ data/src/main/java/com/best/data/remote/dto/homeinfo/HomeStore.kt
@@ -0,0 +1,15 @@
+package com.best.data.remote.dto.homeinfo
+
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.SerialName
+
+data class HomeStore(
+
+    @SerializedName("id") var id: Int? = null,
+    @SerializedName("is_new") var isNew: Boolean? = null,
+    @SerializedName("title") var title: String = "",
+    @SerializedName("subtitle") var subtitle: String ="",
+    @SerializedName("picture") var picture: String = "",
+    @SerializedName("is_buy") var isBuy: Boolean? = null
+
+)
